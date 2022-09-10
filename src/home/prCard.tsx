36c94cd1cc49110d6c373093c015d269f95e01f4
yuukiiwai <yuuki.20020217@gmail.com>
2022-09-10 23:03:44 +0900

    fix some

--- src/home/prCard.tsx
+++ src/home/prCard.tsx
@@ -10,7 +10,7 @@ interface cardprops{
 const PrCard:FC<cardprops> = (props:cardprops) => {
     return(
         <div className="card h100">
-            <img src={props.img_url} alt={props.name+"のイメージ"} 
+            <img src={props.img_url} alt={"グラフィックカード:"+props.name+"のイメージ"} 
             className="card-imt-top ___card-img"/>
             <div className="card-body">
                 <p className="card-title ___card-txt">{props.name}</p>
